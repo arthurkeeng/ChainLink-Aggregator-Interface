1abd4cc0598469e55e23f408d25fdefd71c2bc2a
keeng arthur <121835767+arthurkeeng@users.noreply.github.com>
2023-09-25 10:22:03 +0100

    Add files via upload

---
+++ EthUsd.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+pragma solidity ^0.8.0;
+
+interface AggregatorV3Interface {
+  function decimals() external view returns (uint8);
+
+  function description() external view returns (string memory);
+
+  function version() external view returns (uint256);
+
+  function getRoundData(
+    uint80 _roundId
+  ) external view returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
+
+  function latestRoundData()
+    external
+    view
+    returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
+}
+
+contract FundMe{
+
+    uint256 public minimumUsd = 50 ;
+
+
+    function getBalanceOfEthUsd() public view returns(uint256){
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+
+        (, int256 answer, , , ) =  priceFeed.latestRoundData();
+        return uint256(answer);
+    }
+}
